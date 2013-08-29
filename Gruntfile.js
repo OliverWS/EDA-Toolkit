a27fa1b0bf0a3dc13e4cdb0486f60c1eab87d16e
OliverWS <OliverWS@gmail.com>
2013-08-29 19:33:35 -0400

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/**\n' +
+                '* EDA Toolkit\n' +
+                '* Copyright 2013 Oliver Wilder-Smith \n' +
+                '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+                '*/\n',
+        // Task configuration.
+        clean: {
+            dist: ['dist']
+        },
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: false
+            },
+            edatoolkit: {
+                src: ['lib/*.js'],
+                dest: 'dist/js/<%= pkg.name %>.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            bootstrap: {
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dest %>']
+                }
+            }
+        }
+
+    });
+
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-recess');
+
+
+    // Test task.
+    grunt.registerTask('test', ['jshint', 'qunit']);
+
+    // JS distribution task.
+    grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+    // CSS distribution task.
+    grunt.registerTask('dist-css', ['recess']);
+
+    // Full distribution task.
+    grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js']);
+
+    // Default task.
+    grunt.registerTask('default', ['test', 'dist']);
+};
