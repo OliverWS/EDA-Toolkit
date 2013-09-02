7a9a0e3a11800fb756e916c481075944e44da84e
Oliver Wilder-Smith <OliverWS@gmail.com>
2013-09-02 14:49:13 -0400

    Update travis.yml, index, and automated deploy script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,8 +2,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages with results of travis-ci build\n"
   #copy data we're interested in to other place
   cp -R dist $HOME/dist
-  cp -R css $HOME/css
-  cp -f index.html $HOME/index.html
   #go to home and setup git
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -12,12 +10,10 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/OliverWS/EDA-Toolkit.git  gh-pages > /dev/null
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  cp -Rf $HOME/dist/* .
-  cp -f $HOME/index.html .
-  cp -Rf $HOME/css .
+  cp -Rf $HOME/dist/* app
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
-  echo -e "Done magic with eda-toolkit\n"
+  echo -e "Done with automated deploy of eda-toolkit\n"
 fi
