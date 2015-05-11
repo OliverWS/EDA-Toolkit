5316dc4870d97a2c6ddf4dd0939d79ea0e744150
Travis <travis@travis-ci.org>
2015-05-11 19:41:48 +0000

    Travis build 83 pushed to gh-pages

--- app/js/eda_toolkit.js
+++ app/js/eda_toolkit.js
@@ -950,7 +950,7 @@ var Dropzone = function(el,callback,opts) {
 	dropzone.ondrop = function(e) {	
 		e.preventDefault();
 		if(that.allowFolders){
-			var entry = that.getFiles(e)[0].webkitGetAsEntry();
+			var entry = e.dataTransfer.items[0].webkitGetAsEntry();
 			if(entry.isDirectory){
 				if (document.getElementById("foldername")) {
 					document.getElementById("foldername").innerHTML = entry.name ;
@@ -982,14 +982,7 @@ var Dropzone = function(el,callback,opts) {
 
 		}
 	};
-	that.getFiles = function(e) {
-		if(window.safari != undefined){
-			return e.dataTransfer.files;
-		}
-		else{
-			return e.dataTransfer.items;
-		}
-	}
+	
 	that.traverseFileTree = function(item, path) {
 	  path = path || "";
 	  if (item.isFile && item.name[0] != ".") {
@@ -3112,4 +3105,4 @@ var Grapher = function(div, opts) {
 
 
 
-var version = {build:151}
+var version = {build:150}
